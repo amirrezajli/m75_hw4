9fd5dfabcd4f0a227ac92d87ddadf2a8c2d7b737
amirrezajalili <pouyagorge74@gmail.com>
2022-04-14 19:15:36 +0430

    soale1

---
+++ 1.js
@@ -0,0 +1,9 @@
+let zero = new Number(0);        
+if (zero) {                         
+ alert( "zero is truthy!?!" );
+ 
+}
+
+ //the output is tru & show alert,in console.log output is an object => {0} , because we use new Number(0)
+
+ // if we use let zero= 0 , if(0)the output is false
